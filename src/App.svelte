661601294fb171474402fe39be09c0207ba9b52b
sozonome <berserk.nate@gmail.com>
2020-06-06 14:39:43 +0700

    ✨Styling, Add Illustrations

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,17 @@
 <script>
-  export let name;
   export let searchInput = null;
 
   let fetching = false;
   let show = false;
 
   let randomAdvice;
-	let search = false;
-	let notFound = false;
-
-  function updateName(nameInput) {
-    name = nameInput;
-  }
+  let search = false;
+  let notFound = false;
 
   function fetchRandomAdvice() {
     fetching = true;
-		show = false;
-		notFound= false;
-    console.log(searchInput);
+    show = false;
+    notFound = false;
     if (searchInput !== null && searchInput !== "") {
       search = true;
       fetch(`https://api.adviceslip.com/advice/search/${searchInput}`)
@@ -29,19 +23,18 @@
           } else {
             randomAdvice = data.message.text;
             show = true;
-						search = false;
-						notFound = true;
+            search = false;
+            notFound = true;
           }
         });
     } else {
-      console.log("random");
       search = false;
       fetch(`https://api.adviceslip.com/advice`)
         .then((result) => result.json())
         .then((data) => {
           randomAdvice = data.slip.advice;
           show = true;
-        })
+        });
     }
   }
 </script>
@@ -52,8 +45,20 @@
   footer {
     text-align: center;
     padding: 1em;
-    max-width: 240px;
+    max-width: 280px;
     margin: 0 auto;
+  }
+
+  header img {
+    max-width: 240px;
+  }
+
+  #adviceBox {
+    margin: 2rem 0;
+  }
+
+  #adviceBox img {
+    max-height: 160px;
   }
 
   #name,
@@ -65,6 +70,7 @@
     color: #ff3e00;
     font-size: 2em;
     font-weight: 400;
+    margin-bottom: 0;
   }
 
   .requestButton {
@@ -74,9 +80,10 @@
   }
 
   .adviceText {
-    font-weight: 600;
+    font-weight: 700;
     font-size: 1.5rem;
     margin: 6px 0;
+    font-style: italic;
   }
 
   .requestWarningText,
@@ -86,9 +93,12 @@
 
   .twitter-share-button {
     background-color: rgb(15, 133, 202);
+    width: 100%;
     border-radius: 5px;
-    color: white;
     padding: 5px;
+  }
+  .twitter-share-button a {
+    color: white;
   }
 
   footer p {
@@ -99,88 +109,85 @@
     font-size: 0.7rem;
   }
 
-  @media (min-width: 640px) {
+  @media screen and (min-width: 640px) {
     main {
-      max-width: 800px;
+      max-width: 600px;
     }
   }
 </style>
 
 <header>
   <h4>Advice Generator</h4>
+  <img src="img/croods.png" alt="Header" />
 </header>
 
 <main>
-  <section>
-    <h1>Hello, {name}!</h1>
-    <input
-      id="name"
-      on:input={(e) => updateName(e.target.value)}
-      type="text"
-      placeholder="Your name here" />
-  </section>
-
-  <section>
-    <p>What are you up to today?</p>
-    <button class="requestButton" on:click={() => fetchRandomAdvice()}>
-      Give me an Advice
-    </button>
-    <p style="font-size:0.8rem">
-      Use this search box below to get advice by specific word
-    </p>
-    <input
-      id="searchInput"
-      on:input={(e) => (searchInput = e.target.value)}
-      type="text"
-      value={searchInput} />
-  </section>
+  {#if !fetching}
+    <section id="mainHeader">
+      <h1>Hello, mate!</h1>
+      <p>What are you up to today?</p>
+    </section>
+    <section id="mainSearch">
+      <input
+        id="searchInput"
+        on:input={(e) => (searchInput = e.target.value)}
+        type="text"
+        value={searchInput} />
+      <p style="font-size:0.8rem">
+        Use the search box above to get advice by specific word
+      </p>
+      <button class="requestButton" on:click={() => fetchRandomAdvice()}>
+        Give me an Advice
+      </button>
+    </section>
+  {/if}
 
   {#if fetching}
-    {#if !show}
-      <p>Please Wait...</p>
-    {/if}
-    {#if show}
-      <button
-        on:click={() => {
-          show = false;
-          fetching = false;
-          randomAdvice = undefined;
-          searchInput = '';
-        }}>
-        Clear
-      </button>
-      {#if search}
-        {#each randomAdvice as entry}
-          <p class="adviceText">"{entry.advice}"</p>
+    <section id="adviceBox">
+      {#if !show}
+        <p>Please Wait...</p>
+      {/if}
+      {#if show}
+        {#if search}
+          <p>Search Query : {searchInput}</p>
+          {#each randomAdvice as entry}
+            <p class="adviceText">"{entry.advice}"</p>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={`https://twitter.com/intent/tweet?text=${randomAdvice}`}>
+              <button class="twitter-share-button">Tweet</button>
+            </a>
+            <p class="adviceDate">{entry.date}</p>
+          {/each}
+        {:else if !notFound}
+          <p class="adviceText">"{randomAdvice}"</p>
           <a
-            class="twitter-share-button"
             target="_blank"
             rel="noopener noreferrer"
-            href={`https://twitter.com/intent/tweet?text=${entry.advice}`}>
-            Tweet
+            href={`https://twitter.com/intent/tweet?text=${randomAdvice}`}>
+            <button class="twitter-share-button">Tweet</button>
           </a>
-          <p class="adviceDate">{entry.date}</p>
-        {/each}
-      {:else}
-				{#if !notFound}
-        	<p class="adviceText">"{randomAdvice}"</p>
-					<a
-						class="twitter-share-button"
-						target="_blank"
-						rel="noopener noreferrer"
-						href={`https://twitter.com/intent/tweet?text=${randomAdvice}`}>
-						Tweet
-					</a>
-					<p class="requestWarningText">
-						Wait for a few seconds before another
-						<br />
-						request to get a different one.
-					</p>
-				{:else}
-					<p>{randomAdvice}</p>
-				{/if}
+          <p class="requestWarningText">
+            Wait for a few seconds before another
+            <br />
+            request to get a different one.
+          </p>
+        {:else}
+          <p>{randomAdvice}</p>
+        {/if}
+        <img src="/img/croods-01.png" alt="Hello" />
       {/if}
-    {/if}
+    </section>
+    <button
+      on:click={() => {
+        show = false;
+        fetching = false;
+        randomAdvice = undefined;
+        searchInput = '';
+      }}>
+      Clear
+    </button>
   {/if}
 </main>
 
